4827e5e5b61b4fc2c01569ec803d696b64ca4da3
César David Parada Castillo <cesard.0925@gmail.com>
2023-06-06 17:28:14 +0200

    feat: creacion de controlador y ruta para crear tareas, peticion de postman conseguida

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -2,6 +2,38 @@ const tareaController = {};
 const {Usuario, Tarea_Proyecto, Lista} = require("../models");
 const {sendSuccsessResponse, sendErrorResponse} = require("../_util/sendResponse");
 
+// crear tareas como usuario
+tareaController.createTarea = async (req, res) => {
+    try {
+      const usuario = await Usuario.findOne({
+        where: { id: req.usuario_id },
+      });
+      const { titulo, descripcion, tarea,  } = req.body;
+      const nuevaTarea = await Lista.create({
+        titulo: titulo,
+        descripcion: descripcion,
+        tarea: tarea,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+      const id_proyecto= req.params.id;
+      const tarea_proyecto = await Tarea_Proyecto.create({
+        id_lista: nuevaTarea.id,
+        id_usuario: usuario.id,
+        id_proyecto: id_proyecto,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+  
+      return sendSuccsessResponse(res, 200, {
+        message: "tarea creada",
+        nuevaTarea,
+      });
+    } catch (error) {
+      return sendErrorResponse(res, 500, "No se puede crear la tarea", error);
+    }
+  };
+
 //ver tareas como usuario
 tareaController.getTareas = async (req, res) => {
   try {
--- routes/tareas.js
+++ routes/tareas.js
@@ -7,4 +7,7 @@ const tareaController = require("../controllers/tareaController")
 //ver proyectos como usuario
 router.get("/tarea", verifyToken, isUser, tareaController.getTareas);
 
+//Crear tareas
+router.post("/cratetarea/:id", verifyToken, isUser, tareaController.createTarea);
+
 module.exports = router
